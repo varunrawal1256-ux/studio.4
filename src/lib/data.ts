e882bc7c92f47653ab7d61be627e2b2de7e45726
Manisha Rawal <rawalpayal0987@gmail.com>
2025-11-03 16:59:11 +0000

    Remove Rohit Sharma Virat Kohli name

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -6,21 +6,4 @@ const playerAvatars = PlaceHolderImages.reduce((acc, img) => {
   return acc;
 }, {} as Record<string, string>);
 
-export const initialPlayers: Player[] = [
-  {
-    id: "1",
-    name: "Virat Kohli",
-    avatar: playerAvatars["player1"],
-    matches: 205,
-    runs: 6283,
-    wickets: 4,
-  },
-  {
-    id: "2",
-    name: "Rohit Sharma",
-    avatar: playerAvatars["player2"],
-    matches: 213,
-    runs: 5611,
-    wickets: 15,
-  },
-];
+export const initialPlayers: Player[] = [];
